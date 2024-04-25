bd2a32e1186c65b7efa9a4b9bd27320fd5d6f1ca
Tylwyz <tylwyz1@gmail.com>
2024-04-25 10:52:09 -0500

    Add content for front end, added needed pictures, started on some functionality of the apis

---
+++ weatherapp/src/api.js
@@ -0,0 +1,12 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ae23262e04mshae4d0913cdbc8a9p190586jsn077cba9d2738',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8c58f8c730be85aeda65104d8cc0c0db";
